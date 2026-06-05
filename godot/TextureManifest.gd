99be4c52cd25d4c9cc289e5a6192bba6f19037d9
TheOneWhoJustIsLazy <jennajennets@gmail.com>
2026-06-05 11:54:12 +0200

    Add texture manifest for uploaded materials

---
+++ godot/TextureManifest.gd
@@ -0,0 +1,6 @@
+extends Resource
+class_name TextureManifest
+
+const WALLPAPER := "res://assets/textures/wallpaper_yellow_color.png"
+const CARPET_PLACEHOLDER := "res://assets/textures/carpet_dirty_color.png"
+const CEILING_PLACEHOLDER := "res:"
