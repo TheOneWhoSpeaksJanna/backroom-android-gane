acc971c2fa59de91e7f39f7e29c34bfc8bdbd55a
TheOneWhoJustIsLazy <jennajennets@gmail.com>
2026-06-06 15:10:52 +0200

    Simplify Android branding for Unity Cloud compatibility

--- UnityProject/Assets/Editor/DesolationAndroidBranding.cs
+++ UnityProject/Assets/Editor/DesolationAndroidBranding.cs
@@ -1,5 +1,4 @@
 #if UNITY_EDITOR
-using System;
 using UnityEditor;
 using UnityEditor.Build;
 using UnityEditor.Build.Reporting;
@@ -8,67 +7,37 @@ using UnityEngine;
 [InitializeOnLoad]
 public static class DesolationAndroidBranding
 {
-    const string IconPath = "Assets/AppIcon.jpg";
+    const string IconPath = "Assets/AppIcon.png";
 
     static DesolationAndroidBranding()
     {
         EditorApplication.delayCall += Apply;
     }
 
-    [MenuItem("Desolation/Apply Android Branding And Import Settings")]
+    [MenuItem("Desolation/Apply Android Branding")]
     public static void Apply()
     {
-        AssetDatabase.ImportAsset(IconPath, ImportAssetOptions.ForceUpdate);
-        TextureImporter ti = AssetImporter.GetAtPath(IconPath) as TextureImporter;
-        if (ti != null)
+        TextureImporter importer = AssetImporter.GetAtPath(IconPath) as TextureImporter;
+        if (importer != null)
         {
-            ti.textureType = TextureImporterType.Default;
-            ti.mipmapEnabled = false;
-            ti.alphaIsTransparency = false;
-            ti.maxTextureSize = 1024;
-            ti.textureCompression = TextureImporterCompression.CompressedHQ;
-            ti.SaveAndReimport();
+            importer.textureType = TextureImporterType.Default;
+            importer.mipmapEnabled = false;
+            importer.alphaIsTransparency = true;
+            importer.maxTextureSize = 1024;
+            importer.textureCompression = TextureImporterCompression.CompressedHQ;
+            importer.SaveAndReimport();
         }
 
         Texture2D icon = AssetDatabase.LoadAssetAtPath<Texture2D>(IconPath);
         if (icon != null)
         {
-            Texture2D[] groupIcons = new Texture2D[8];
-            for (int i = 0; i < groupIcons.Length; i++) groupIcons[i] = icon;
-            PlayerSettings.SetIconsForTargetGroup(BuildTargetGroup.Android, groupIcons);
-            try
-            {
-                PlatformIconKind[] kinds = PlayerSettings.GetPlatformIconKindsForTargetGroup(BuildTargetGroup.Android);
-                foreach (PlatformIconKind kind in kinds)
-                {
-                    PlatformIcon[] platformIcons = PlayerSettings.GetPlatformIcons(BuildTargetGroup.Android, kind);
-                    for (int i = 0; i < platformIcons.Length; i++) platformIcons[i].SetTexture(icon);
-                    PlayerSettings.SetPlatformIcons(BuildTargetGroup.Android, kind, platformIcons);
-                }
-            }
-            catch (Exception)
-            {
-            }
+            Texture2D[] icons = new Texture2D[] { icon, icon, icon, icon, icon, icon, icon, icon };
+            PlayerSettings.SetIconsForTargetGroup(BuildTargetGroup.Android, icons);
         }
 
         PlayerSettings.companyName = "Desolation";
         PlayerSettings.productName = "Desolation: The Backrooms";
         PlayerSettings.SetApplicationIdentifier(BuildTargetGroup.Android, "com.desolation.thebackrooms");
-
-        foreach (string guid in AssetDatabase.FindAssets("t:AudioClip", new[] { "Assets" }))
-        {
-            string path = AssetDatabase.GUIDToAssetPath(guid);
-            AudioImporter importer = AssetImporter.GetAtPath(path) as AudioImporter;
-            if (importer == null) continue;
-            AudioImporterSampleSettings settings = importer.defaultSampleSettings;
-            settings.loadType = AudioClipLoadType.CompressedInMemory;
-            settings.compressionFormat = AudioCompressionFormat.Vorbis;
-            settings.quality = .58f;
-            importer.defaultSampleSettings = settings;
-            importer.forceToMono = true;
-            importer.SaveAndReimport();
-        }
-
         AssetDatabase.SaveAssets();
     }
 }
