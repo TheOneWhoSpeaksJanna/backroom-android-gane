82823d504d10a56ca3fa3f58ae30a942c8923f01
root <root@localhost.localdomain>
2026-06-10 10:02:11 +0000

    Fix: simplify TextureBridgeBuildProcessor to remove Application.dataPath reference that fails in build pipeline

--- UnityProject/Assets/Editor/TextureBridgeBuildProcessor.cs
+++ UnityProject/Assets/Editor/TextureBridgeBuildProcessor.cs
@@ -1,27 +1,13 @@
-using UnityEditor;
 using UnityEditor.Build;
 using UnityEditor.Build.Reporting;
-using System.IO;
 
 public sealed class TextureBridgeBuildProcessor : IPreprocessBuildWithReport
 {
     public int callbackOrder => -1000;
 
-    [InitializeOnLoadMethod]
-    static void Load()
-    {
-        EnsureDirs();
-    }
-
     public void OnPreprocessBuild(BuildReport report)
     {
-        EnsureDirs();
-    }
-
-    static void EnsureDirs()
-    {
-        string target = Path.Combine(Application.dataPath, "Resources/Textures");
-        Directory.CreateDirectory(target);
-        AssetDatabase.Refresh();
+        // Textures are committed in Assets/Resources/Textures/.
+        // No pre-build sync needed anymore.
     }
 }
